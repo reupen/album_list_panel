4e9417429a33c74b4862f9427c4e3403e31c3102
msquared2 <msquared2@users.noreply.github.com>
2015-11-29 16:27:39 +0000

    move dependencies to git submodule

--- foo_uie_albumlist/stdafx.h
+++ foo_uie_albumlist/stdafx.h
@@ -2,15 +2,15 @@
 #define OEMRESOURCE
 #define _WIN32_WINNT _WIN32_WINNT_VISTA
 
-#include "../SDK/foobar2000.h"
-#include "../helpers/helpers.h"
+#include "../common/foobar2000/SDK/foobar2000.h"
+#include "../common/foobar2000/helpers/helpers.h"
 #include "resource.h"
 #include <commctrl.h>
 #include <windowsx.h>
 
-#include "../columns_ui-sdk/ui_extension.h"
-#include "../ui_helpers/stdafx.h"
-#include "../mmh/stdafx.h"
+#include "../common/columns_ui-sdk/ui_extension.h"
+#include "../common/ui_helpers/stdafx.h"
+#include "../common/mmh/stdafx.h"
 
 using namespace pfc;
 
