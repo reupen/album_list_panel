a70f771b649ca50b6092d9ce4b0b1707b22a17f4
Reupen Shah <reupen@users.noreply.github.com>
2017-07-30 21:10:02 +0100

    Tidy up config.cpp/config.h

--- foo_uie_albumlist/config.h
+++ foo_uie_albumlist/config.h
@@ -1,88 +1,94 @@
 #pragma once
 
-class cfg_view_list_t : public cfg_var
-{
-private:
-    struct entry
-    {
-        string8 name;
-        string8 value;
-        entry(const char * p_name, const char * p_value) : name(p_name), value(p_value) {}
-        entry() {}
-    };
-    list_t<entry> data;
+class cfg_view_list_t : public cfg_var {
 public:
+    void get_data_raw(stream_writer* out, abort_callback& p_abort) override;
+    void set_data_raw(stream_reader* r, size_t psize, abort_callback& p_abort) override;
 
-    ~cfg_view_list_t() {}
-    void get_data_raw(stream_writer * out, abort_callback & p_abort) override;
-    void set_data_raw(stream_reader * r, unsigned psize, abort_callback & p_abort) override;
+    void reset();
 
-    virtual void reset();
-
-    const char * get_name(unsigned idx) const
+    const char* get_name(size_t index) const
     {
-        if (idx >= 0 && idx<data.get_count()) return data[idx].name;
-        else return nullptr;
+        if (index >= 0 && index < m_data.get_count())
+            return m_data[index].name;
+        return nullptr;
     }
 
-    const char * get_value(unsigned idx) const
+    const char* get_value(size_t index) const
     {
-        if (idx >= 0 && idx<data.get_count()) return data[idx].value;
-        else return nullptr;
+        if (index >= 0 && index < m_data.get_count())
+            return m_data[index].value;
+        return nullptr;
     }
 
-    unsigned find_item(const char * name) const
+    size_t find_item(const char* name) const
     {
-        unsigned n, m = data.get_count();
-        for (n = 0; n<m; n++) if (!stricmp_utf8(data[n].name, name)) return n;
-        return (unsigned)(-1);
+        const auto count = m_data.get_count();
+        for (size_t i{0}; i < count; i++)
+            if (!stricmp_utf8(m_data[i].name, name))
+                return i;
+
+        return (std::numeric_limits<size_t>::max)();
     }
 
-    unsigned add_item(const char * name, const char * value)
+    size_t add_item(const char* name, const char* value)
     {
-        return data.add_item(entry(name, value));
+        return m_data.add_item(entry{name, value});
     }
 
-    void remove_item(unsigned idx)
+    void remove_item(size_t index)
     {
-        data.remove_by_idx(idx);
+        m_data.remove_by_idx(index);
     }
 
     void remove_all()
     {
-        data.remove_all();
+        m_data.remove_all();
     }
 
-    void modify_item(unsigned idx, const char * name, const char * value)
+    void modify_item(size_t index, const char* name, const char* value)
     {
-        entry & p_entry = data[idx];
+        auto& p_entry = m_data[index];
         p_entry.name = name;
         p_entry.value = value;
     }
 
-    unsigned get_count() const { return data.get_count(); }
+    size_t get_count() const
+    {
+        return m_data.get_count();
+    }
 
-    void swap(unsigned idx1, unsigned idx2) { data.swap_items(idx1, idx2); }
+    void swap(size_t index1, size_t index2)
+    {
+        m_data.swap_items(index1, index2);
+    }
 
-    cfg_view_list_t(const GUID & p_guid) : cfg_var(p_guid)
+    cfg_view_list_t(const GUID& p_guid) : cfg_var(p_guid)
     {
         reset();
     }
 
-    void format_display(unsigned n, string_base & out) const
+    void format_display(size_t index, string_base& out) const
     {
-        out = get_name(n);
+        out = get_name(index);
         out += " : ";
-        out += get_value(n);
+        out += get_value(index);
     }
+
+private:
+    struct entry {
+        pfc::string8 name;
+        pfc::string8 value;
+    };
+
+    pfc::list_t<entry> m_data;
 };
 
-
 extern const GUID g_guid_album_list_font,
-    g_guid_album_list_colours;
+    g_guid_album_list_colours,
+    g_guid_preferences_album_list_panel;
 
 extern cfg_view_list_t cfg_view_list;
-extern GUID g_guid_preferences_album_list_panel;
 extern cfg_bool cfg_themed;
 extern cfg_int cfg_populate,
     cfg_autosend,
@@ -103,4 +109,3 @@ extern cfg_int cfg_populate,
     cfg_item_height,
     cfg_custom_item_height;
 extern cfg_string cfg_playlist_name;
-
