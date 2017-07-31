9a99732add5c7c59972efd9eaf7d66a976a913f9
Reupen Shah <reupen@users.noreply.github.com>
2017-07-31 20:52:42 +0100

    Tidy up actions.cpp

--- foo_uie_albumlist/actions.cpp
+++ foo_uie_albumlist/actions.cpp
@@ -1,67 +1,71 @@
 #include "stdafx.h"
 
-void do_playlist(node_ptr const & src, bool replace, bool b_new)
-{
-    if (src.is_valid() && src->get_entries().get_count()>0)
+class titleformat_hook_view : public titleformat_hook {
+public:
+    bool process_field(titleformat_text_out* p_out, const char* p_name, unsigned p_name_length,
+        bool& p_found_flag) override
     {
-        if (b_new) src->create_new_playlist();
-        else src->send_to_playlist(replace);
-        if (replace && cfg_autoplay)
-        {
+        p_found_flag = false;
+        if (m_view) {
+            if (!stricmp_utf8_ex(p_name, p_name_length, "_view", 5)) {
+                p_out->write(titleformat_inputtypes::meta, m_view, pfc_infinite);
+                p_found_flag = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool process_function(titleformat_text_out* p_out, const char* p_name, unsigned p_name_length,
+        titleformat_hook_function_params* p_params, bool& p_found_flag) override
+    {
+        p_found_flag = false;
+        return false;
+    }
+
+    titleformat_hook_view(const char* p_view) : m_view{p_view} {}
+private:
+    const char* m_view;
+};
+
+void do_playlist(const node_ptr& src, bool replace, bool b_new)
+{
+    if (src.is_valid() && src->get_entries().get_count() > 0) {
+        if (b_new)
+            src->create_new_playlist();
+        else
+            src->send_to_playlist(replace);
+
+        if (replace && cfg_autoplay) {
             static_api_ptr_t<playlist_manager>()->reset_playing_playlist();
             static_api_ptr_t<play_control>()->play_start();
         }
     }
 }
 
-void do_autosend_playlist(node_ptr const & src, string_base & view, bool b_play)
+void do_autosend_playlist(const node_ptr& src, pfc::string_base& view, bool b_play)
 {
-    if (src.is_valid())
-    {
+    if (src.is_valid()) {
         static_api_ptr_t<playlist_manager> api;
         string8 playlist_name;
 
-        class titleformat_hook_view : public titleformat_hook
-        {
-            const char * m_view;
-        public:
-            bool process_field(titleformat_text_out * p_out, const char * p_name, unsigned p_name_length, bool & p_found_flag) override
-            {
-                p_found_flag = false;
-                if (m_view)
-                {
-                    if (!stricmp_utf8_ex(p_name, p_name_length, "_view", 5))
-                    {
-                        p_out->write(titleformat_inputtypes::meta, m_view, pfc_infinite);
-                        p_found_flag = true;
-                        return true;
-                    }
-                }
-                return false;
-            }
-            bool process_function(titleformat_text_out * p_out, const char * p_name, unsigned p_name_length, titleformat_hook_function_params * p_params, bool & p_found_flag) override
-            {
-                p_found_flag = false;
-                return false;
-            }
-            inline titleformat_hook_view(const char * p_view) : m_view(p_view)
-            {};
-        };
-        titleformat_hook_view tf_hook(view);
+        titleformat_hook_view tf_hook{view};
         static_api_ptr_t<titleformat_compiler>()->run(&tf_hook, playlist_name, cfg_playlist_name);
-        //playlist_name.fix_filename_chars(); //for find playlist NOT RELEVANT FOR 0.9
-        unsigned idx = api->find_or_create_playlist(playlist_name, pfc_infinite);
-        api->playlist_undo_backup(idx);
-        api->playlist_clear(idx);
-        if (cfg_add_items_use_core_sort) api->playlist_add_items_filter(idx, src->get_entries(), cfg_add_items_select != 0);
-        else
-        {
+        const auto index = api->find_or_create_playlist(playlist_name, pfc_infinite);
+
+        api->playlist_undo_backup(index);
+        api->playlist_clear(index);
+
+        if (cfg_add_items_use_core_sort)
+            api->playlist_add_items_filter(index, src->get_entries(), cfg_add_items_select != 0);
+        else {
             src->sort_entries();
-            api->playlist_add_items(idx, src->get_entries(), bit_array_val(cfg_add_items_select != 0));
+            api->playlist_add_items(index, src->get_entries(), bit_array_val(cfg_add_items_select != 0));
         }
-        api->set_active_playlist(idx);
-        if (b_play && cfg_autoplay)
-        {
+
+        api->set_active_playlist(index);
+
+        if (b_play && cfg_autoplay) {
             api->reset_playing_playlist();
             static_api_ptr_t<play_control>()->play_start();
         }
