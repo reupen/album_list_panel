c5175e37a7e2f00f424291baf489222c9b2b8b37
Reupen Shah <reupen@users.noreply.github.com>
2017-08-03 21:10:42 +0100

    Tidy up node.h/node.cpp

--- foo_uie_albumlist/node.cpp
+++ foo_uie_albumlist/node.cpp
@@ -2,36 +2,33 @@
 
 void node::sort_children()
 {
-    const auto count = children.get_count();
+    const auto count = m_children.get_count();
     mmh::Permuation permutation(count);
     pfc::array_staticsize_t<pfc::stringcvt::string_wide_from_utf8_fast> sortdata(count);
 
     for (size_t n = 0; n < count; n++)
-        sortdata[n].convert(children[n]->value);
+        sortdata[n].convert(m_children[n]->m_value);
     mmh::sort_get_permuation(sortdata, permutation, StrCmpLogicalW, false, false, true);
 
-    mmh::destructive_reorder(children, permutation);
-    concurrency::parallel_for(size_t{0}, count, [this](size_t n)
-    {
-        children[n]->sort_children();
+    mmh::destructive_reorder(m_children, permutation);
+    concurrency::parallel_for(size_t{0}, count, [this](size_t n) {
+        m_children[n]->sort_children();
     });
 }
 
 void node::sort_entries()//for contextmenu
 {
-    if (!m_sorted)
-    {
+    if (!m_sorted) {
         pfc::string8 tf_string;
-        if (b_bydir)
+        if (m_bydir)
             tf_string = "%path_sort%";
-        else
-        {
-            tf_string = p_dbe->get_hierarchy();
+        else {
+            tf_string = m_window->get_hierarchy();
             tf_string += "|%path_sort%";
         }
         service_ptr_t<titleformat_object> script;
         if (static_api_ptr_t<titleformat_compiler>()->compile(script, tf_string))
-            fbh::sort_metadb_handle_list_by_format(entries, script, nullptr);
+            fbh::sort_metadb_handle_list_by_format(m_tracks, script, nullptr);
         m_sorted = true;
     }
 }
@@ -39,12 +36,12 @@ void node::sort_entries()//for contextmenu
 void node::create_new_playlist()
 {
     static_api_ptr_t<playlist_manager> api;
-    string8 name = value.get_ptr();
-    if (name.is_empty()) name = "All music";
-    unsigned idx = api->create_playlist(name, pfc_infinite, pfc_infinite);
-    if (idx != pfc_infinite)
-    {
-        api->set_active_playlist(idx);
+    string8 name = m_value.get_ptr();
+    if (name.is_empty())
+        name = "All music";
+    const size_t index = api->create_playlist(name, pfc_infinite, pfc_infinite);
+    if (index != pfc_infinite) {
+        api->set_active_playlist(index);
         send_to_playlist(true);
     }
 }
@@ -54,114 +51,107 @@ void node::send_to_playlist(bool replace)
     static_api_ptr_t<playlist_manager> api;
     const bool select = !!cfg_add_items_select;
     api->activeplaylist_undo_backup();
-    if (replace) api->activeplaylist_clear();
-    else if (select) api->activeplaylist_clear_selection();
-    if (cfg_add_items_use_core_sort) api->activeplaylist_add_items_filter(entries, select);
-    else
-    {
+    if (replace)
+        api->activeplaylist_clear();
+    else if (select)
+        api->activeplaylist_clear_selection();
+    if (cfg_add_items_use_core_sort)
+        api->activeplaylist_add_items_filter(m_tracks, select);
+    else {
         sort_entries();
-        api->activeplaylist_add_items(entries, bit_array_val(select));
+        api->activeplaylist_add_items(m_tracks, bit_array_val(select));
     }
 
-    if (select && !replace)
-    {
-        unsigned num = api->activeplaylist_get_item_count();
-        if (num > 0)
-        {
+    if (select && !replace) {
+        const size_t num = api->activeplaylist_get_item_count();
+        if (num > 0) {
             api->activeplaylist_set_focus_item(num - 1);
         }
     }
 }
 
-node::node(const char * p_value, unsigned p_value_len, album_list_window * dbe, uint16_t level)
-    : p_dbe(dbe), m_ti(nullptr), m_label_dirty(false), m_level(level)
+node::node(const char* p_value, unsigned p_value_len, album_list_window* window, uint16_t level)
+    : m_level(level), m_window(window)
 {
-    if (p_value && p_value_len > 0)
-    {
-        value.set_string(p_value, p_value_len);
+    if (p_value && p_value_len > 0) {
+        m_value.set_string(p_value, p_value_len);
     }
     m_sorted = false;
 }
 
-
-void node::remove_entries(pfc::bit_array & mask)
+void node::remove_entries(pfc::bit_array& mask)
 {
-    entries.remove_mask(mask);
+    m_tracks.remove_mask(mask);
 }
 
 void node::set_data(const list_base_const_t<metadb_handle_ptr>& p_data, bool b_keep_existing)
 {
     if (!b_keep_existing)
-        entries.remove_all();
-    entries.add_items(p_data);
+        m_tracks.remove_all();
+    m_tracks.add_items(p_data);
     m_sorted = false;
 }
 
-node_ptr node::find_or_add_child(const char * p_value, unsigned p_value_len, bool b_find, bool & b_new)
+node_ptr node::find_or_add_child(const char* p_value, unsigned p_value_len, bool b_find, bool& b_new)
 {
     if (!b_find)
         return add_child_v2(p_value, p_value_len);
-    if (p_value_len == 0 || *p_value == 0)
-    {
-        p_value = "?"; p_value_len = 1;
+    if (p_value_len == 0 || *p_value == 0) {
+        p_value = "?";
+        p_value_len = 1;
     }
     t_size index = 0;
     b_new = true;
-    if (/*b_find && */children.bsearch_t(g_compare_name, uTS(p_value, p_value_len), index))
-    {
+    if (m_children.bsearch_t(g_compare_name, uTS(p_value, p_value_len), index)) {
         b_new = false;
     }
     else
-        children.insert_item(new node(p_value, p_value_len, p_dbe, m_level + 1), index);
-    return children[index];
+        m_children.insert_item(new node(p_value, p_value_len, m_window, m_level + 1), index);
+    return m_children[index];
 }
 
-node_ptr node::add_child_v2(const char * p_value, unsigned p_value_len)
+node_ptr node::add_child_v2(const char* p_value, unsigned p_value_len)
 {
-    if (p_value_len == 0 || *p_value == 0)
-    {
-        p_value = "?"; p_value_len = 1;
+    if (p_value_len == 0 || *p_value == 0) {
+        p_value = "?";
+        p_value_len = 1;
     }
-    node_ptr temp = new node(p_value, p_value_len, p_dbe, m_level + 1);
-    children.add_item(temp);
+    node_ptr temp = new node(p_value, p_value_len, m_window, m_level + 1);
+    m_children.add_item(temp);
     return temp;
 }
 
 void node::mark_all_labels_dirty()
 {
     m_label_dirty = true;
-    t_size i = children.get_count();
-    for (; i; i--)
-    {
-        children[i - 1]->mark_all_labels_dirty();
+    t_size i = m_children.get_count();
+    for (; i; i--) {
+        m_children[i - 1]->mark_all_labels_dirty();
     }
 }
 
 void node::purge_empty_children(HWND wnd)
 {
-    t_size i = children.get_count(), index_first_removed = pfc_infinite;
-    for (; i; i--)
-    {
-        auto& child = children[i - 1];
-        if (!child->get_entries().get_count())
-        {
-            if (p_dbe && p_dbe->m_selection == child)
-                p_dbe->m_selection = nullptr;
+    size_t index_first_removed = pfc_infinite;
+    for (size_t i = m_children.get_count(); i; i--) {
+        auto& child = m_children[i - 1];
+        if (!child->get_entries().get_count()) {
+            if (m_window && m_window->m_selection == child)
+                m_window->m_selection = nullptr;
             if (child->m_ti)
-                TreeView_DeleteItem(wnd, child->m_ti);
-            children.remove_by_idx(i - 1);
+            TreeView_DeleteItem(wnd, child->m_ti);
+            m_children.remove_by_idx(i - 1);
             index_first_removed = i - 1;
         }
     }
+
     if (index_first_removed != pfc_infinite && cfg_show_numbers)
         m_label_dirty = true;
 
-    if (index_first_removed != pfc_infinite && cfg_show_numbers2)
-    {
-        t_size count = children.get_count();
-        for (i = index_first_removed; i<count; i++)
-        {
-            children[i]->m_label_dirty = true;
+    if (index_first_removed != pfc_infinite && cfg_show_numbers2) {
+        const t_size count = m_children.get_count();
+        for (size_t i = index_first_removed; i < count; i++) {
+            m_children[i]->m_label_dirty = true;
         }
     }
 }
--- foo_uie_albumlist/node.h
+++ foo_uie_albumlist/node.h
@@ -2,91 +2,101 @@
 
 typedef pfc::refcounted_object_ptr_t<class node> node_ptr;
 
-class node : public pfc::refcounted_object_root
-{
-private:
-    string_simple value;
-    list_t<node_ptr> children;
-    metadb_handle_list entries;
-    bool m_sorted;
-    bool b_bydir;
-
-    class album_list_window * p_dbe;
-
-    static int sortproc(const wchar_t* n1, const wchar_t* n2)
-    {
-        return StrCmpLogicalW(n1, n2);
-    }
+class node : public pfc::refcounted_object_root {
 public:
-    HTREEITEM m_ti;
-    bool m_label_dirty;
-    bool m_children_inserted = false;
+    HTREEITEM m_ti{};
+    bool m_label_dirty{};
+    bool m_children_inserted{};
     uint16_t m_level;
 
+    node(const char* p_value, unsigned p_value_len, class album_list_window* window, uint16_t level);
+
     void sort_children();
-
     void sort_entries();//for contextmenu
-
-    const metadb_handle_list & get_entries() const { return entries; }
+    
+    const metadb_handle_list& get_entries() const
+    {
+        return m_tracks;
+    }
 
     void create_new_playlist();
-
     void send_to_playlist(bool replace);
 
-    inline const char * get_sort_data() const { return value; }
-
-    node(const char * p_value, unsigned p_value_len, class album_list_window * dbe, uint16_t level);
-    inline void set_bydir(bool p) { b_bydir = p; }
+    void set_bydir(bool p)
+    {
+        m_bydir = p;
+    }
 
     ~node() override
     {
-        entries.remove_all();
-        children.remove_all();
+        m_tracks.remove_all();
+        m_children.remove_all();
     }
 
-    const char * get_val()
+    const char* get_val()
     {
-        return value.is_empty() ? "All music" : (const char*)value;
+        return m_value.is_empty() ? "All music" : m_value.get_ptr();
     }
 
-    void add_entry(const metadb_handle_ptr & p_entry)
+    void add_entry(const metadb_handle_ptr& p_entry)
     {
-        entries.add_item(p_entry);
+        m_tracks.add_item(p_entry);
     }
 
-    void remove_entries(pfc::bit_array & mask);
+    void remove_entries(pfc::bit_array& mask);
 
-    void set_data(const list_base_const_t<metadb_handle_ptr> & p_data, bool b_keep_existing);
+    void set_data(const list_base_const_t<metadb_handle_ptr>& p_data, bool b_keep_existing);
 
-    static int g_compare_name(const node_ptr & p1, const wchar_t * str)
+    static int g_compare_name(const node_ptr& p1, const wchar_t* str)
     {
-        return StrCmpLogicalW(uT(p1->value), str);
+        return StrCmpLogicalW(uT(p1->m_value), str);
     }
 
-    node_ptr find_or_add_child(const char * p_value, unsigned p_value_len, bool b_find, bool & b_new);
+    node_ptr find_or_add_child(const char* p_value, unsigned p_value_len, bool b_find, bool& b_new);
 
-    node_ptr add_child_v2(const char * p_value, unsigned p_value_len);
-    node_ptr add_child_v2(const char * p_value)
+    node_ptr add_child_v2(const char* p_value, unsigned p_value_len);
+
+    node_ptr add_child_v2(const char* p_value)
     {
         return add_child_v2(p_value, strlen(p_value));
     }
 
-    void reset() { children.remove_all(); entries.remove_all(); m_sorted = false; }
+    void reset()
+    {
+        m_children.remove_all();
+        m_tracks.remove_all();
+        m_sorted = false;
+    }
 
     void mark_all_labels_dirty();
 
     void purge_empty_children(HWND wnd);
 
-    const list_t<node_ptr> & get_children() const { return children; }
-    list_t<node_ptr> & get_children() { return children; }
+    const list_t<node_ptr>& get_children() const
+    {
+        return m_children;
+    }
+
+    list_t<node_ptr>& get_children()
+    {
+        return m_children;
+    }
 
     unsigned get_num_children() const
     {
-        return children.get_count();
-    }
-    unsigned get_num_entries() const
-    {
-        return entries.get_count();
+        return m_children.get_count();
     }
 
+    unsigned get_num_entries() const
+    {
+        return m_tracks.get_count();
+    }
+
+private:
+    string_simple m_value;
+    list_t<node_ptr> m_children;
+    metadb_handle_list m_tracks;
+    bool m_sorted{};
+    bool m_bydir{};
+    class album_list_window* m_window{};
 };
