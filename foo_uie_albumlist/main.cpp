reupen/album_list_panel

commit c7890d78893f2bc7e880066d30cc00e166e41f09

Add ClickAction enum and refactor related code

foo_uie_albumlist/main.cpp
@@ -2,6 +2,7 @@
 #include "menu.h"
 #include "node.h"
 #include "tree_view_populator.h"
+#include "actions.h"
 
 //TODO: node name as field
 
@@ -409,6 +410,24 @@ void album_list_window::get_menu_items(ui_extension::menu_hook_t& p_hook)
     p_hook.add_node(ui_extension::menu_node_ptr(new menu_node_select_view(this)));
     p_hook.add_node(node_filter);
     p_hook.add_node(node_settings);
+}
+
+void album_list_window::do_click_action(ClickAction click_action)
+{
+    switch (click_action) {
+    case ClickAction::send_to_playlist:
+        do_playlist(m_selection, true);
+        break;
+    case ClickAction::add_to_playlist:
+        do_playlist(m_selection, false);
+        break;
+    case ClickAction::send_to_new_playlist:
+        do_playlist(m_selection, true, true);
+        break;
+    case ClickAction::send_to_autosend_playlist:
+        do_autosend_playlist(m_selection, m_view, true);
+        break;
+    }
 }
 
 // {606E9CDD-45EE-4c3b-9FD5-49381CEBE8AE}

foo_uie_albumlist/main.h
@@ -6,6 +6,14 @@
 
 extern const char* directory_structure_view_name;
 
+enum class ClickAction {
+    none_or_default,
+    send_to_playlist,
+    add_to_playlist,
+    send_to_new_playlist,
+    send_to_autosend_playlist
+};
+
 class album_list_window : public ui_extension::container_ui_extension, public library_callback_dynamic {
     friend class font_notify;
     friend class node;
@@ -84,6 +92,8 @@ private:
     static const GUID s_extension_guid;
     static const char* s_class_name;
     static HFONT s_font;
+
+    void do_click_action(ClickAction click_action);
 
     HWND m_wnd_tv{nullptr};
     HWND m_wnd_edit{nullptr};

foo_uie_albumlist/main_hook_proc.cpp
@@ -133,20 +133,7 @@ LRESULT WINAPI album_list_window::on_hook(HWND wnd, UINT msg, WPARAM wp, LPARAM 
             if (cfg_middle_click_action) {
                 TreeView_SelectItem(wnd, ti.hItem);
 
-                switch (cfg_middle_click_action) {
-                case 1:
-                    do_playlist(m_selection, true);
-                    break;
-                case 2:
-                    do_playlist(m_selection, false);
-                    break;
-                case 3:
-                    do_playlist(m_selection, true, true);
-                    break;
-                case 4:
-                    do_autosend_playlist(m_selection, m_view, true);
-                    break;
-                }
+                do_click_action(static_cast<ClickAction>(cfg_middle_click_action.get_value()));
             }
         }
         break;
@@ -161,24 +148,7 @@ LRESULT WINAPI album_list_window::on_hook(HWND wnd, UINT msg, WPARAM wp, LPARAM 
 
             if (ti.flags & TVHT_ONITEM) {
 
-                switch (cfg_double_click_action) {
-                case 0:
-                    do_playlist(m_selection, true);
-                    return 0;
-                case 1:
-                    do_playlist(m_selection, true);
-                    return 0;
-                case 2:
-                    do_playlist(m_selection, false);
-                    return 0;
-                case 3:
-                    do_playlist(m_selection, true, true);
-                    return 0;
-                case 4:
-                    do_autosend_playlist(m_selection, m_view, true);
-                    return 0;
-                }
-                break;
+                do_click_action(static_cast<ClickAction>(cfg_double_click_action.get_value()));
             }
         }
     }

commit 5b111fd7a62fbb28619b6803131454e69b6f2ee8

Stop triggering expand/collapse on custom double-click actions
#23

This fixes a regression introduced in c7890d78893f2bc7e880066d30cc00e166e41f09 that caused the default expand/collapse double-click action to occur when a custom double-click action was set (in addition to the custom action set).

foo_uie_albumlist/main.cpp
@@ -449,7 +449,7 @@ void album_list_window::get_menu_items(ui_extension::menu_hook_t& p_hook)
     p_hook.add_node(node_settings);
 }
 
-void album_list_window::do_click_action(ClickAction click_action)
+bool album_list_window::do_click_action(ClickAction click_action)
 {
     switch (click_action) {
     case ClickAction::send_to_playlist:
@@ -464,7 +464,10 @@ void album_list_window::do_click_action(ClickAction click_action)
     case ClickAction::send_to_autosend_playlist:
         do_autosend_playlist(m_selection, m_view, true);
         break;
+    default:
+        return false;
     }
+    return true;
 }
 
 // {606E9CDD-45EE-4c3b-9FD5-49381CEBE8AE}

foo_uie_albumlist/main.h
@@ -94,7 +94,7 @@ private:
     static const char* s_class_name;
     static HFONT s_font;
 
-    void do_click_action(ClickAction click_action);
+    bool do_click_action(ClickAction click_action);
 
     HWND m_wnd_tv{nullptr};
     HWND m_wnd_edit{nullptr};

foo_uie_albumlist/main_hook_proc.cpp
@@ -147,10 +147,13 @@ LRESULT WINAPI album_list_window::on_hook(HWND wnd, UINT msg, WPARAM wp, LPARAM 
             TreeView_HitTest(wnd, &ti);
 
             if (ti.flags & TVHT_ONITEM) {
+                const auto click_processed = do_click_action(static_cast<ClickAction>(cfg_double_click_action.get_value()));
 
-                do_click_action(static_cast<ClickAction>(cfg_double_click_action.get_value()));
+                if (click_processed)
+                    return 0;
             }
         }
+        break;
     }
     return CallWindowProc(m_treeproc, wnd, msg, wp, lp);
 }