reupen/album_list_panel

commit 3c4f2d52b0abcb1fc2633ccee1423c58291d69c7

Delay initialisation until after media library initialisation
Populating the tree in one go is much faster.

foo_uie_albumlist/main_tree_builder.cpp
@@ -474,18 +474,20 @@ void album_list_window::remove_nodes(metadb_handle_list_t<pfc::alloc_fast_aggres
 
 void album_list_window::on_items_added(const pfc::list_base_const_t<metadb_handle_ptr>& p_const_data)
 {
-    const auto timer = pfc::hires_timer::create_and_start();
+    if (m_library_v4.is_valid() && !m_library_v4->is_initialized())
+        return;
 
     metadb_handle_list_t<pfc::alloc_fast_aggressive> to_add = p_const_data;
     metadb_handle_list_t<pfc::alloc_fast_aggressive> to_remove;
 
     update_tree(to_add, to_remove, true);
-
-    m_initialisation_time += timer.query();
 }
 
 void album_list_window::on_items_removed(const pfc::list_base_const_t<metadb_handle_ptr>& p_data_const)
 {
+    if (m_library_v4.is_valid() && !m_library_v4->is_initialized())
+        return;
+
     metadb_handle_list_t<pfc::alloc_fast_aggressive> to_add;
     metadb_handle_list_t<pfc::alloc_fast_aggressive> to_remove = p_data_const;
 
@@ -501,21 +503,16 @@ void album_list_window::on_items_modified(const pfc::list_base_const_t<metadb_ha
 
 void album_list_window::on_items_modified_v2(metadb_handle_list_cref items, metadb_io_callback_v2_data& data)
 {
+    if (m_library_v4->is_initialized())
+        return;
+
     on_items_modified(items);
 }
 
 void album_list_window::on_library_initialized()
 {
-    const auto timer = pfc::hires_timer::create_and_start();
-
-    m_node_state.reset();
-    if (m_wnd_tv && m_populated) {
-        enable_tree_view();
-        restore_scroll_position();
-
-        m_initialisation_time += timer.query();
-        console::print("Album list panel: initialised in ", pfc::format_float(m_initialisation_time, 0, 3), " s");
-    }
+    if (cfg_populate_on_init)
+        refresh_tree();
 }
 
 void album_list_window::update_tree(metadb_handle_list_t<pfc::alloc_fast_aggressive>& to_add,
@@ -565,9 +562,7 @@ void album_list_window::update_tree(metadb_handle_list_t<pfc::alloc_fast_aggress
 
     m_populated = true;
 
-    if (m_library_v4.is_valid() && m_library_v4->is_initialized()) {
-        enable_tree_view();
-    }
+    m_node_state.reset();
 
     redraw_on_reset.reset();
 
@@ -581,6 +576,9 @@ void album_list_window::refresh_tree()
     if (!m_wnd_tv)
         return;
 
+    if (m_library_v4.is_valid() && !m_library_v4->is_initialized())
+        return;
+
     metadb_handle_list_t<pfc::alloc_fast_aggressive> to_add;
     metadb_handle_list_t<pfc::alloc_fast_aggressive> to_remove;
     to_add.prealloc(1024);
@@ -591,6 +589,5 @@ void album_list_window::refresh_tree()
 
     update_tree(to_add, to_remove, false);
 
-    if (!m_library_v4.is_valid() || m_library_v4->is_initialized())
-        console::print("Album list panel: initialised in ", pfc::format_float(timer.query(), 0, 3), " s");
+    console::print("Album list panel: initialised in ", pfc::format_float(timer.query(), 0, 3), " s");
 }

foo_uie_albumlist/main.cpp
@@ -379,11 +379,6 @@ void album_list_window::create_tree()
     if (!cfg_show_horizontal_scroll_bar)
         styles |= TVS_NOHSCROLL;
 
-    m_enabled = !m_library_v4.is_valid() || m_library_v4->is_initialized();
-
-    if (!m_enabled)
-        styles |= WS_DISABLED;
-
     m_wnd_tv = CreateWindowEx(ex_styles, WC_TREEVIEW, L"Album list", styles, 0, 0, 0, 0, wnd,
         reinterpret_cast<HMENU>(IDC_TREE), core_api::get_my_instance(), nullptr);
 
@@ -479,14 +474,6 @@ void album_list_window::restore_scroll_position()
     if (is_initialised) {
         m_saved_scroll_position.reset();
     }
-}
-
-void album_list_window::enable_tree_view()
-{
-    if (!m_enabled)
-        EnableWindow(m_wnd_tv, TRUE);
-
-    m_enabled = true;
 }
 
 void album_list_window::get_config(stream_writer* p_writer, abort_callback& p_abort) const

foo_uie_albumlist/main.h
@@ -110,8 +110,6 @@ private:
 
     LRESULT WINAPI on_tree_hooked_message(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
 
-    void enable_tree_view();
-
     static inline pfc::ptr_list_t<album_list_window> s_instances;
     static const GUID s_extension_guid;
     static const char* s_class_name;
@@ -126,7 +124,6 @@ private:
     WNDPROC m_treeproc{nullptr};
     bool m_initialised{false};
     bool m_populated{false};
-    bool m_enabled{};
     bool m_dragging{false};
     bool m_clicked{false};
     bool m_filter{false};
@@ -141,7 +138,6 @@ private:
     search_filter::ptr m_filter_ptr;
     ui_selection_holder::ptr m_selection_holder;
     NodeFormatter m_node_formatter;
-    double m_initialisation_time{};
     library_manager_v4::ptr m_library_v4;
     library_manager_v3::ptr m_library_v3;
 };

commit fd32b05417f87814861d63816f1f0ddfc72cf54d

Fix handling of tracks being modified on foobar2000 2.0 and newer
A regression in 3c4f2d52b0abcb1fc2633ccee1423c58291d69c7 caused the modifications to tracks in the media library to not be handled on foobar2000 2.0 and newer.

foo_uie_albumlist/main_tree_builder.cpp
@@ -503,7 +503,7 @@ void AlbumListWindow::on_items_modified(const pfc::list_base_const_t<metadb_hand
 
 void AlbumListWindow::on_items_modified_v2(metadb_handle_list_cref items, metadb_io_callback_v2_data& data)
 {
-    if (m_library_v4->is_initialized())
+    if (!m_library_v4->is_initialized())
         return;
 
     on_items_modified(items);